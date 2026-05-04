0a3d4bacb62cfeb2f0c5712b184f73ae9bf62d64
Awdhesh Kumar Soni <awdheshsoni.official@gmail.com>
2026-05-04 07:15:21 +0530

    Add files via upload

---
+++ sessions/c++ practice session - 1.cpp
@@ -0,0 +1,22 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello";
+
+    return 0;
+}
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "How to push this code to git hub?"<<endl;
+    
+    return 0;
+}
